1afe8b60210c7d31398344f9c46cdce636847c06
Igor Gritsenko <gerkuless@bigmir.net>
2016-10-23 22:23:57 +0300

    19th lesson. skeleton and start:watchify

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//19th lesson
+// git init инициализируем репозиторий
+//git remote add origin 'https://...git'
+//git pull origin master достать, если есть че нового
+//.gitignore или https://habrahabr.ru/post/202696/ Правильные способы исключения файлов в Git
 //git rm -r -f .idea удалить из коммита
+//git add -A все добавить
+//git status посмтреть что закоммичено и не
+//git commit -m 'blabla'
+//git push origin master
+
+//npm init инициалиируем app
+//npm i -g browserify
+//npm i --save babelify
